cee0131dccf96efd18ab2d4224ab1213b0fda79c
harkanni <harkanzyempire001@gmail.com>
2024-08-04 21:52:27 +0100

    my trip home page done

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import { Colors } from '@/constants/Colors'
 
 const TabLayout = () => {
-  return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name='mytrip' />
-      <Tabs.Screen name='discover' />
-      <Tabs.Screen name='profile' />
-    </Tabs>
-  )
+   return (
+      <Tabs screenOptions={{ headerShown: false, tabBarActiveTintColor: Colors.PRIMARY }}>
+         <Tabs.Screen options={{ tabBarIcon: ({ color }) => <Ionicons name='location-sharp' size={24} color={color} />, tabBarLabel: 'My Trip', }} name='mytrip' />
+         <Tabs.Screen options={{ tabBarIcon: ({ color }) => <Ionicons name='globe-sharp' size={24} color={color} />, tabBarLabel: 'Discover', }} name='discover' />
+         <Tabs.Screen options={{ tabBarIcon: ({ color }) => <Ionicons name='people-circle' size={24} color={color} />, tabBarLabel: 'Profile', }} name='profile' />
+      </Tabs>
+   )
 }
 
 export default TabLayout
--- app/(tabs)/mytrip.tsx
+++ app/(tabs)/mytrip.tsx
@@ -1,12 +1,26 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { Colors } from '@/constants/Colors'
+import { Ionicons } from '@expo/vector-icons'
+import StartNewTripCard from '@/components/MyTrips/StartNewTripCard'
 
 const MyTrip = () => {
-  return (
-    <View>
-      <Text>MyTrip</Text>
-    </View>
-  )
+   const [userTrips, setUserTrips] = useState([]);
+   return (
+      <View style={{
+         padding: 25,
+         paddingTop: 50,
+         backgroundColor: Colors.WHITE,
+         height: '100%'
+      }}>
+         <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+            <Text style={{ fontFamily: 'outfit-bold', fontSize: 30, fontWeight: 'bold'}}>My Trips</Text>
+            <Ionicons name='add-circle' size={45} color={'black'} />
+         </View>
+
+         {userTrips.length == 0 ? <StartNewTripCard /> : null}
+      </View>
+   )
 }
 
 export default MyTrip
